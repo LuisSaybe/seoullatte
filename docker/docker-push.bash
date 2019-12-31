9b503903f7eab7779dcc091b5d22063efb1ddfaa
Luis <luis@trois.io>
2019-12-31 19:01:49 +0100

    build(travis)

--- docker/docker-push.bash
+++ docker/docker-push.bash
@@ -2,8 +2,8 @@
 
 if [ $TRAVIS_BRANCH == "master" ]
 then
-  envsubst < web/js/settings-production.json > web/js/settings-production-copy.json
-  cat web/js/settings-production-copy.json > web/js/settings-production.json
+  envsubst < web/js/settings/production.json > web/js/settings/production-copy.json
+  cat web/js/settings/production-copy.json > web/js/settings/production.json
 
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
   docker build --rm -f docker/Dockerfile -t luissaybe/joseon .
