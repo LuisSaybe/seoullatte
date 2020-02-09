f6383f0cace33dd48dbecf8bce5a6ae3d220bfab
Luis <luis@trois.io>
2020-02-09 22:50:21 +0100

    build(travis)

--- docker/docker-push.bash
+++ docker/docker-push.bash
@@ -6,7 +6,7 @@ then
   cat web/js/settings/production-copy.json > web/js/settings/production.json
 
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-  docker build --rm -f docker/Dockerfile -t luissaybe/joseon .
-  docker push luissaybe/joseon:latest
+  docker build --rm -f docker/Dockerfile -t luissaybe/seoullatte .
+  docker push luissaybe/seoullatte:latest
   docker logout
 fi
