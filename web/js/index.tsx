b88503b19b637c7ecb3bc9e42afc2fa88c007f13
Luis E. Banegas Saybe <luis@trois.io>
2020-08-14 22:44:19 -0400

    chore(yarn)

--- web/js/index.tsx
+++ web/js/index.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "react-toastify/dist/ReactToastify.css";
-
 import { Root } from "web/js/page/root";
 import "web/js/sentry";
 import "web/js/i18n";
--- web/js/page/root/index.tsx
+++ web/js/page/root/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { toast, ToastContainer } from "react-toastify";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
@@ -9,30 +8,16 @@ import { Application } from "web/js/component/application";
 import { useDeviceSupported } from "web/js/hook/useDeviceSupported";
 import { DeviceNotSupported } from "web/js/page/device-not-supported";
 import { reducers } from "web/js/redux/reducer";
-import styles from "./style.scss";
 
 export function Root() {
   const store = createStore(reducers);
   const deviceSupported = useDeviceSupported();
 
   return (
-    <>
-      <BrowserRouter>
-        <Provider store={store}>
-          {deviceSupported ? <Application /> : <DeviceNotSupported />}
-          <ToastContainer
-            position={toast.POSITION.BOTTOM_LEFT}
-            bodyClassName={styles.toaster}
-            hideProgressBar
-          />
-        </Provider>
-      </BrowserRouter>
-      <Helmet>
-        <meta
-          property="og:image"
-          content="https://luissaybe.nyc3.digitaloceanspaces.com/seoul-latte/images/icon/192x192.png"
-        />
-      </Helmet>
-    </>
+    <BrowserRouter>
+      <Provider store={store}>
+        {deviceSupported ? <Application /> : <DeviceNotSupported />}
+      </Provider>
+    </BrowserRouter>
   );
 }
